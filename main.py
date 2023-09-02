93771f5a1fe574a9adbf679d8c1c60836bb8e3cd
geo-mateo <mateusz.kaczmarek32@gmaial.com>
2023-09-02 17:25:43 +0200

    third commit

--- main.py
+++ main.py
@@ -3,25 +3,29 @@ import random
 print("Welcome to Hangman!")
 
 words = ["hacker", "bounty", "random"]
-#Create an empty list
-#For each letter in the secret_word add a "_" that will be
-#         printed to the console
-#Example if the word is Hacker "_","_","_","_","_","_"
+
 
 secret_word = random.choice(words)
+print(secret_word)
 display_word = []
-for lett in secret_word:
+for letter in secret_word:
     display_word += "_"
 print(display_word)
 
-guess = input("Geuss a letter").lower()
-#Loop through each if the letters in the chosen word
-#if the letter is in the word replace the "_" with the letter
-#it should look like this "_","a","c","_","_","r"
 
+#1 use a while loop word has been keeps going
+# until the word has been guessed
 
-for position in range(len(secret_word)):
-    letter = secret_word[position]
-    if letter == guess:
-        display_word[position] = letter
-print(display_word)
+game_over = False
+
+while not game_over:
+    guess = input("Geuss a letter").lower()
+    for position in range(len(secret_word)):
+        letter = secret_word[position]
+        if letter == guess:
+            display_word[position] = letter
+    print(display_word)
+
+    if "_" not in display_word:
+        print("You Won")
+        game_over = True
